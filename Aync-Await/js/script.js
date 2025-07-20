b7540720e059e30868682bc398d8e29d0ec8a2d8
M3NGSZE <mengse.thun13@gmail.com>
2025-07-21 03:34:54 +0700

    fetch function

--- Aync-Await/js/script.js
+++ Aync-Await/js/script.js
@@ -1,1 +1,15 @@
 "use strict"
+
+import { cardProduct } from "../components/cardProduct.js"
+
+const renderArea = document.querySelector("#render-area")
+
+const BASE_URL = "http://localhost:5501/Aync-Await/data/products.json"
+
+fetch(BASE_URL)
+    .then((res) => res.json())
+    .then((data) => {
+        data.map((product) => {
+            renderArea.innerHTML += cardProduct(product)
+        });
+    })
