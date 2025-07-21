105b47a95fd2ffb9b5c042247176c14a8d8088b8
M3NGSZE <mengse.thun13@gmail.com>
2025-07-21 21:36:42 +0700

    typescript part 2

--- TypeScript-Fundamental/dist/index.js
+++ TypeScript-Fundamental/dist/index.js
@@ -2,3 +2,17 @@
 let a = 22;
 console.log("I'm", a);
 a = 30;
+let x = ['a', 'b', 'c'];
+let person;
+person = [1, "mengse"];
+let staff = {
+    id: 1,
+    name: "megnse",
+    details: function () {
+        return `ID: ${this.id}, Name: ${this.name}`;
+    },
+    retirement: function (date) {
+        console.log(`${this.name} is retiring on ${date}`);
+    }
+};
+staff.retirement(new Date());
--- TypeScript-Fundamental/src/index.ts
+++ TypeScript-Fundamental/src/index.ts
@@ -1,6 +1,39 @@
-let a: Number = 22;
+let a: number = 22;
 console.log("I'm", a)
 
 a = 30
 
+let x: string[] = ['a', 'b', 'c']
+
 // this comment to improve readability
+
+// compile both typescript and javascript file if both true
+// tsc && node ./dist/index.js
+// clear && tsc && node ./dist/index.js  || clear and run
+
+// tuple
+let person: [number, string]
+person = [1, "mengse"]
+
+const enum Sizes {small, meduim, large}
+
+// console.log(Sizes)
+
+let staff: {
+    readonly id: number,
+    name: string,
+    salary?: number,
+    details: () => string,
+    retirement: (date: Date) => void
+} = {
+    id: 1,
+    name: "megnse",
+    details: function(){
+        return `ID: ${this.id}, Name: ${this.name}`
+    },
+    retirement: function(date: Date){
+        console.log(`${this.name} is retiring on ${date}`)
+    }
+}
+staff.retirement(new Date());
+// console.log()
